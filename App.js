e8b365db3f0a38e0e18e32159d4126a071286ec2
adit4git <adityamunjuluri@gmail.comm>
2021-05-31 15:28:11 -0400

    basic layout setup for ios and android

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { StatusBar, StyleSheet, Text, View, SafeAreaView, Platform } from "react-native";
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.search}>search</Text>
+        <Text style={styles.result}>list</Text>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,    
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {   
+    backgroundColor: "green",
+    padding: 16,
+  },
+  result: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "blue",
+    padding: 16,
   },
 });
